9c671694c4f1beb41a45171967205ccad9d748b7
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-11-20 11:53:02 -0800

    Completed Then-Catch part

--- file_reader.js
+++ file_reader.js
@@ -1,10 +1,26 @@
 const fs = require("fs").promises;
 
 // THEN-CATCH SOLUTION BELOW THIS LINE
-
-
-
-
-
+let firstname, lastname, age, hobbies;
+fs.readFile('firstname.txt', 'utf-8')
+  .then( data => {
+    firstname = data;
+    return fs.readFile('lastname.txt', 'utf-8');
+  })
+  .then( data => {
+    lastname = data;
+    return fs.readFile('age.txt', 'utf-8');
+  })
+  .then( data => {
+    age = data;
+    return fs.readFile('hobbies.txt', 'utf-8');
+  })
+  .then( data => {
+    hobbies = JSON.parse(data);
+    console.log(`${firstname} ${lastname} is ${age} years old and his hobbies are ${hobbies[0]} and ${hobbies[1]}.`)
+  })
+  .catch(err => {
+    console.error(`THEN-CATCH Error: ${err}`);
+  })
 
 // ASYNC/AWAIT SOLUTION BELOW THIS LINE
